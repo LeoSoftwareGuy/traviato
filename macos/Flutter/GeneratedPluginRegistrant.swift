be97360d29068e2e539980f815f4a589a48b1675
leo <leoengineeratwork@oulook.com>
2026-08-30 13:08:54 +0300

    chore(deps): register new plugins for desktop platforms #65
    flutter pub get's plugin registration for flutter_local_notifications,
    flutter_timezone (Linux/Windows/macOS registrants + cmake plugin lists).

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,8 @@ import Foundation
 import app_links
 import file_selector_macos
 import flutter_image_compress_macos
+import flutter_local_notifications
+import flutter_timezone
 import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
@@ -16,6 +18,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
